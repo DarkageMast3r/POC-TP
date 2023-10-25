3a599fbf4176e2ab54b0e31fe724b86092ef652f
unknown <tijmen.peterson@solcon.nl>
2023-10-25 15:25:04 +0200

    Fully functional

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import BaggingClassifier
 from sklearn.feature_extraction.text import *
 import pickle
 import fileinput
@@ -29,23 +30,53 @@ emails = pd.read_csv("emails.csv")
 
 
 
-def print_screen(categories, email):
-    ui.clear()
-    ui.draw_categories(0, 0, 20, ui.height-15, categories)
-
-    ui.set_bounds(20, 0, ui.width, ui.height)
-    ui.draw_string(21, 1, ui.width-22, email)
-    ui.draw_box(20, 0, ui.width-20, ui.height)
-
-    ui.draw_categories(0, ui.height-15, 20, 15, ["Categorize", "New category", "Delete category"]);
-
-    ui.draw()
 
 
 categories = []
 categories_sorted = {}
 all_emails = []
 categorized = []
+
+def predict(email):
+
+    global classifier
+    global vectorizer
+
+    vectorizer = CountVectorizer()
+    all_emails.append(email)
+    data = {}
+    data["text"] = all_emails
+    dataframe = pd.DataFrame(data=data)
+    # Make sure the new features are supported
+    model.sanitize_dataframe(dataframe)
+
+    vectorizer.fit(dataframe.text)
+
+
+    all_emails.pop()
+
+    data = {}
+    data["text"] = all_emails
+    data["category"] = categorized
+    dataframe = pd.DataFrame(data=data)
+
+    
+    model.sanitize_dataframe(dataframe)
+    vectors = vectorizer.transform(dataframe.text)
+
+    # Fit classifier to new features
+    # Optimization: Don't do this if no new features are introduced
+    classifier.fit(vectors, dataframe.category)
+
+    data = {}
+    data["text"] = [email]
+    dataframe = pd.DataFrame(data=data)
+
+
+    vectors = vectorizer.transform(dataframe)
+    predicted = classifier.predict(vectors)
+    return predicted[0]
+
 
 def categorize_email(email, category): 
     try:
@@ -54,15 +85,19 @@ def categorize_email(email, category):
     except:
         return
 
-    print("Learning...")
+
     all_emails.append(email)
     categorized.append(int(category)-1)
-    vectors = model.vectorize([email], vectorizer)
+    
     data = {}
     data["text"] = all_emails
     data["category"] = categorized
     dataframe = pd.DataFrame(data=data)
-    print(dataframe)
+
+    vectors = model.vectorize(dataframe, vectorizer)
+
+    classifier = model.create(n_classes = 5, n_features = 1)
+    classifier.fit(vectors, dataframe.category)
 
 
 def category_create(title):
@@ -82,6 +117,23 @@ def category_destroy(category):
         elif email_categories[email] == int(category)-1:
             email_categories[email] = None
 
+def print_screen(categories, email):
+    ui.clear()
+    ui.draw_categories(0, 0, 20, ui.height-15, categories)
+
+    ui.set_bounds(20, 0, ui.width, ui.height)
+    ui.draw_string(21, 1, ui.width-22, email)
+    ui.draw_box(20, 0, ui.width-20, ui.height)
+
+    print(categorized)
+    if len(categorized) > 0:
+        predicted = predict(email)
+        #        ui.set_bounds()
+        ui.draw_string(21, ui.height-2, ui.width-22, "Predicted: "+categories[predicted])
+
+    ui.draw_categories(0, ui.height-15, 20, 15, ["Categorize", "New category", "Delete category"]);
+
+    ui.draw()
 
 
 
